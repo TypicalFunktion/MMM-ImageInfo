f147bab609214d55131697819dcba486fd3d0ac4
TypicalFunktion <100875615+TypicalFunktion@users.noreply.github.com>
2025-04-15 12:36:56 -0600

    Update MMM-ImageInfo.js

--- MMM-ImageInfo.js
+++ MMM-ImageInfo.js
@@ -1,7 +1,7 @@
 /* Magic Mirror
  * Module: MMM-ImageInfo
  * 
- * By TypicalFunktion
+ * By MagicMirror Community
  * MIT Licensed.
  */
 
@@ -13,7 +13,9 @@ Module.register("MMM-ImageInfo", {
         headerText: "", // No header by default
         showFileName: true,
         showCreationDate: true,
+        showLocation: true, // New option to show location
         dateFormat: "MMMM D, YYYY", // Format for displaying the date
+        locationDateFormat: "[{location} - ]{date}", // Format for combined location and date
         textClass: "small", // CSS class for styling the text
         wallpaperSelector: ".MMM-Wallpaper img", // CSS selector for finding the wallpaper image
     },
@@ -35,7 +37,13 @@ Module.register("MMM-ImageInfo", {
             path: "",
             filename: "",
             src: "",
-            creationDate: null
+            creationDate: null,
+            location: {
+                city: null,
+                state: null,
+                country: null,
+                hasLocation: false
+            }
         };
         this.loaded = false;
         this.lastCheckedSrc = "";
@@ -50,8 +58,8 @@ Module.register("MMM-ImageInfo", {
         wrapper.className = "image-info";
 
         if (!this.loaded) {
-            wrapper.innerHTML = "Getting Image Data";
-            wrapper.className = "highlight light xsmall";
+            wrapper.innerHTML = "Waiting for image data...";
+            wrapper.className = "dimmed light small";
             return wrapper;
         }
 
@@ -71,12 +79,51 @@ Module.register("MMM-ImageInfo", {
             infoContainer.appendChild(filenameElem);
         }
 
-        if (this.config.showCreationDate && this.imageInfo.creationDate) {
-            var dateElem = document.createElement("div");
-            dateElem.className = "image-date";
-            var formattedDate = moment(this.imageInfo.creationDate).format(this.config.dateFormat);
-            dateElem.innerHTML = formattedDate;
-            infoContainer.appendChild(dateElem);
+        // Format location and date if configured
+        if ((this.config.showCreationDate && this.imageInfo.creationDate) || 
+            (this.config.showLocation && this.imageInfo.location.hasLocation)) {
+            
+            var dateLocElem = document.createElement("div");
+            dateLocElem.className = "image-date-location";
+            
+            // Format the location string if we have location data
+            var locationText = "";
+            if (this.config.showLocation && this.imageInfo.location.hasLocation) {
+                // Format as City, State - or just City if no state
+                if (this.imageInfo.location.city && this.imageInfo.location.state) {
+                    locationText = this.imageInfo.location.city + ", " + this.imageInfo.location.state;
+                } else if (this.imageInfo.location.city) {
+                    locationText = this.imageInfo.location.city;
+                } else if (this.imageInfo.location.state) {
+                    locationText = this.imageInfo.location.state;
+                }
+                
+                // Add country if available and not already using state
+                if (this.imageInfo.location.country && !this.imageInfo.location.state) {
+                    locationText += locationText ? ", " + this.imageInfo.location.country : this.imageInfo.location.country;
+                }
+            }
+            
+            // Format the date if available
+            var dateText = "";
+            if (this.config.showCreationDate && this.imageInfo.creationDate) {
+                dateText = moment(this.imageInfo.creationDate).format(this.config.dateFormat);
+            }
+            
+            // Combine location and date according to the format
+            var displayText = this.config.locationDateFormat
+                .replace("{location}", locationText)
+                .replace("{date}", dateText);
+                
+            // Clean up any artifacts if location or date is missing
+            displayText = displayText
+                .replace(/\[\s*-\s*\]/g, "") // Remove empty brackets with dash
+                .replace(/\[\s*\]/g, "")     // Remove empty brackets
+                .replace(/\s+-\s+/g, "")     // Remove lone dashes
+                .trim();                     // Remove extra whitespace
+                
+            dateLocElem.innerHTML = displayText;
+            infoContainer.appendChild(dateLocElem);
         }
 
         wrapper.appendChild(infoContainer);
@@ -138,9 +185,15 @@ Module.register("MMM-ImageInfo", {
         if (notification === "IMAGE_INFO_RESULT") {
             Log.info("MMM-ImageInfo: Received image info", payload);
             
-            // Update with creation date
+            // Update with creation date and location
             if (payload.filename === this.imageInfo.filename) {
                 this.imageInfo.creationDate = payload.creationDate;
+                this.imageInfo.location = payload.location || {
+                    city: null,
+                    state: null,
+                    country: null,
+                    hasLocation: false
+                };
                 this.loaded = true;
                 this.updateDom();
             }
